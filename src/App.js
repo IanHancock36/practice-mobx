d1b5ce43d0da02d6024b3828ea3493f972a7e17d
Ian Hancock <northroadian@gmail.com>
2021-07-05 14:18:28 -0500

    adding store provider intial test the functionality works!

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {useLocalStore, useObserver} from 'mobx-react'
+
+const StoreContext = React.createContext();
+// store provider wraps around all children components 
+const StoreProvider = ({children})=> {
+  const store = useLocalStore(() => ({
+    dinosaurs : ['Triceratops']
+   }))
+
+
+return (<StoreContext.Provider value ={store}>{children}</StoreContext.Provider>
+
+)
+  }
+   
+// useContext is a hook that allows you to use 
+  const DinosaurList = () => {
+    const store = React.useContext(StoreContext)
+
+     return (
+       <ul>
+         {store.dinosaurs.map(dinosaur => <li key={dinosaur}>{dinosaur}</li>)}
+       </ul>
+     )
+  }
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <StoreProvider>
+     <main>
+       <DinosaurList />
+     </main>
+    </StoreProvider>
+    
   );
 }
 
 export default App;
+
+
+// so this is giving the context or what ever is in the store provider the ability so that the children throughout the app can use this context where
+  // where ever it is wanted or needed to be used. 
+
